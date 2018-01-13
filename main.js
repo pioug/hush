e81b1da1c9acd1dbd87866a61dbb4a82094e5d4a
Gilles Piou <gilles.piou@gmail.com>
2018-01-14 04:51:11 +0800

    Enable copy-paste shortcuts

--- main.js
+++ main.js
@@ -162,6 +162,17 @@ function createMenu() {
       role: 'quit'
     }]
   }, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+  }, {
     label: 'Playback',
     submenu: [{
       label: 'Random',
